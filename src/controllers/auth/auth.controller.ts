1517824b74b3ae27e31aff2a7902b53c218fb6d6
Abdullah Al MahMud <abdudevs@gmail.com>
2025-01-01 18:07:25 +0600

    feat: centralized login

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -7,40 +7,48 @@ import {
   signInEntityService,
   signUpAdminService,
 } from "../../services";
-import { sendResponse, staticProps } from "../../utils";
+import { ENUM_ADMIN_ROLES, sendResponse, staticProps } from "../../utils";
 import { IMulterFiles } from "../../interfaces";
-
-export const CentralSignin: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const parsedData = req.body;
-
-    let result;
-
-    if (parsedData.school_uid === staticProps.default.DEFAULT_SUPER_ADMIN) {
-      result = await signInAdminService(parsedData);
-    } else {
-      result = await signInEntityService(parsedData);
-    }
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      message: staticProps.common.LOGGED_IN,
-      data: result,
-    });
-  }
-);
 
 export const CentralSignup: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
     const parsedData = req.body;
     const { single } = req.files as IMulterFiles;
+    const { entity_type } = parsedData;
 
     let result;
 
-    if (parsedData.school_uid === staticProps.default.DEFAULT_SUPER_ADMIN) {
+    if (
+      entity_type === ENUM_ADMIN_ROLES.SUPER_ADMIN ||
+      entity_type === ENUM_ADMIN_ROLES.STAFF_ADMIN
+    ) {
       result = await signUpAdminService(parsedData, single);
     } else {
       result = await signUpEntityService(parsedData, single);
+    }
+
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
+      message: staticProps.common.CREATED,
+      data: result,
+    });
+  }
+);
+
+export const CentralSignin: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const parsedData = req.body;
+    const { entity_type } = parsedData;
+
+    let result;
+
+    if (
+      entity_type === ENUM_ADMIN_ROLES.SUPER_ADMIN ||
+      entity_type === ENUM_ADMIN_ROLES.STAFF_ADMIN
+    ) {
+      result = await signInAdminService(parsedData);
+    } else {
+      result = await signInEntityService(parsedData);
     }
 
     sendResponse(res, {
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,12 +1,61 @@
-import { IAdminCreate, IAdminSignin, IUploadFile } from "../../interfaces";
+import httpStatus from "http-status";
+import { ApiError } from "../../cores";
+import { IUploadFile } from "../../interfaces";
+import {
+  signInSchoolAdminService,
+  signInStudentService,
+  signInTeacherService,
+  signUpSchoolAdminService,
+  signUpStudentService,
+  signUpTeacherService,
+} from "../secondary";
+import { ENUM_SCHOOL_ROLES, staticProps } from "../../utils";
 
 export const signUpEntityService = async (
-  adminData: IAdminCreate,
+  schoolEntityData: any,
   file: IUploadFile[] | undefined
 ) => {
-  //check students then teachers then school admins to find the user
+  const { entity_type } = schoolEntityData;
+  let entity = null;
+
+  //try to create entity on student
+  if (entity_type === ENUM_SCHOOL_ROLES.STUDENT) {
+    entity = await signUpStudentService(schoolEntityData, file);
+  } else if (
+    entity_type === ENUM_SCHOOL_ROLES.TEACHER ||
+    entity_type === ENUM_SCHOOL_ROLES.HEAD_TEACHER
+  ) {
+    entity = await signUpTeacherService(schoolEntityData, file);
+  } else if (entity_type === ENUM_SCHOOL_ROLES.SCHOOL_ADMIN) {
+    entity = await signUpSchoolAdminService(schoolEntityData);
+  }
+
+  if (!entity) {
+    throw new ApiError(httpStatus.NOT_FOUND, staticProps.common.NOT_CREATED);
+  }
+
+  return entity;
 };
 
-export const signInEntityService = async (adminData: IAdminSignin) => {
-  //check students then teachers then school admins to find the user
+export const signInEntityService = async (schoolEntityData: any) => {
+  const { entity_type } = schoolEntityData;
+  let entity = null;
+
+  //try to signin entity
+  if (entity_type === ENUM_SCHOOL_ROLES.STUDENT) {
+    entity = await signInStudentService(schoolEntityData);
+  } else if (
+    entity_type === ENUM_SCHOOL_ROLES.TEACHER ||
+    entity_type === ENUM_SCHOOL_ROLES.HEAD_TEACHER
+  ) {
+    entity = await signInTeacherService(schoolEntityData);
+  } else if (entity_type === ENUM_SCHOOL_ROLES.SCHOOL_ADMIN) {
+    entity = await signInSchoolAdminService(schoolEntityData);
+  }
+
+  if (!entity) {
+    throw new ApiError(httpStatus.NOT_FOUND, staticProps.common.NOT_FOUND);
+  }
+
+  return entity;
 };
